cb42f2a834371164ca94b731e6b49ad34c3f9f57
Cside <cside.story@gmail.com>
2023-09-19 18:47:53 +0900

    fix popup message

--- src/popup.ts
+++ src/popup.ts
@@ -1,7 +1,11 @@
 import { addReviewUrls } from '@cside/browser-extension-utils';
 import 'bootstrap/dist/css/bootstrap.css';
 import ids from '../ids.json';
+import manifest from '../manifest.json';
 
 document.addEventListener('DOMContentLoaded', () => {
   addReviewUrls(chrome.runtime.id, ids);
+  const h1 = document.getElementsByTagName('h1')[0];
+  if (!h1) throw new Error('h1 is not found');
+  h1.append(manifest.name);
 });
